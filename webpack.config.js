e8627025cdde0c072794e73cb1f3c12903b11761
mollymillk <kristey.z39@gmail.com>
2021-09-21 11:55:09 +0300

    company info container

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack')
 const CssMinimizerWebpackPligin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -8,6 +9,13 @@ var miniSVGDataURI = require('mini-svg-data-uri');
 
 module.exports = {
 	mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+	},
 	entry: './index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -55,10 +63,12 @@ module.exports = {
         new CopyWebpackPlugin({
             patterns: [
                 { from: "components/header/img", to: "img" },
-				{from: "components/main_container/img", to: "img"}
+				{from: "components/main_container/img", to: "img"},
+				{from: "components/company_info/img", to: "img"}
               ],
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
 
 	],
 	optimization: {
